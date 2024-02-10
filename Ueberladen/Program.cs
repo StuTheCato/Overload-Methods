ad353dfeb7261fd0f695ee6c6a795e44886e3563
StuTheCato <stuat.@Stuats-MacBook-Air.local>
2024-02-10 20:23:18 +0100

    Initial commit

---
+++ Ueberladen/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ueberladen
+{
+    internal class Program
+    {
+        static void AusgabeGanzzahl(int zahl)
+        {
+            Console.WriteLine($"Die Ganzzahl lautet: {zahl}");
+        }
+
+        static void AusgabeDezimal(double wert)
+        {
+            Console.WriteLine($"Die Dezimalzahl lautet {wert}");
+        }
+
+        static void AusgabeText(string text)
+        {
+            Console.WriteLine($"Der Text lautet: {text}");
+        }
+
+        static void Main(string[] args)
+        {
+            AusgabeGanzzahl(11);
+            AusgabeDezimal(3.5);
+            AusgabeText("Hello world");
+
+            Console.ReadKey();
+        }
+    }
+}
